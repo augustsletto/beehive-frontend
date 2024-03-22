6012b3d5fcddf1b7ceea84580f5970de63a9760f
august sletto <augustsletto@gmail.com>
2024-03-22 18:53:34 +0000

    deployment error fix

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://beehive-social-3ec964865be0.herokuapp.com/"
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
